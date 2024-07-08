741abf49ec3f7989748bb02e5fa3ea8515784450
dani <danigilangsoejana@gmail.com>
2024-07-09 06:07:01 +0700

    lupa routesnya

---
+++ routes/auth.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const { body } = require('express-validator');
+const validate = require('../middlewares/validate');
+const authController = require('../controllers/authController');
+const router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: User authentication
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Invalid input
+ */
+router.post('/register', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 6 })
+], validate, authController.register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       401:
+ *         description: Invalid email or password
+ */
+router.post('/login', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 6 })
+], validate, authController.login);
+
+module.exports = router;
